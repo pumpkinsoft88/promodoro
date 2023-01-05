0d6574feeec638ae5755f056d25afa1f471a63d5
seungwoo shin <pumpkinsoft88@gmail.com>
2023-01-05 13:26:47 +0900

    7th commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -48,79 +48,105 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void onResetPressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+      totalSeconds = twentyFiveMinutes;
+    });
+  }
+
   String format(int seconds){
     var duration = Duration(seconds: seconds);
     // print(duration.toString().split(".").first.substring(2,7));
     return duration.toString().split(".").first.substring(2,7);
   }
 
-  @override
+
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(children: [
-        Flexible(
-          flex: 1,
-          child: Container(
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              format(totalSeconds),
-              style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 89,
-                  fontWeight: FontWeight.w600),
-            ),
-          ),
-        ),
-        Flexible(
-          flex: 3,
-          child: Center(
-            child: IconButton(
-              iconSize: 120,
-              color: Theme.of(context).cardColor,
-              onPressed:  isRunning? onPausePressed 
-              : onStartPressed,
-              icon: Icon(isRunning 
-              ? Icons.pause_circle_outline 
-              : Icons.play_circle_outline),
-            ),
-          ),
-        ),
-        Flexible(
-          flex: 1,
-          child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(color: Theme.of(context).cardColor,
-                  borderRadius: BorderRadius.circular(50),),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Pomodoros',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          color: Theme.of(context).textTheme.headline1!.color,
-                        ),
-                      ),
-                      Text(
-                        '$totalPomodoros',
-                        style: TextStyle(
-                          fontSize: 58,
-                          fontWeight: FontWeight.w600,
-                          color: Theme.of(context).textTheme.headline1!.color,
-                        ),
-                      )
-                    ],
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  format(totalSeconds),
+                  style: TextStyle(
+                    color: Theme.of(context).cardColor,
+                    fontSize: 89,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
-            ],
-          ),
-        )
-      ]),
-    );
+            ),
+            Flexible(
+              flex: 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    iconSize: 120,
+                    color: Theme.of(context).cardColor,
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
+                    icon: isRunning
+                        ? const Icon(Icons.pause_circle_outline)
+                        : const Icon(Icons.play_circle_outline),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  IconButton(
+                    iconSize: 50,
+                    color: Theme.of(context).cardColor,
+                    onPressed: onResetPressed,
+                    icon: const Icon(Icons.reset_tv_outlined),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Pomodoros',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color:
+                                  Theme.of(context).textTheme.headline1!.color,
+                            ),
+                          ),
+                          Text(
+                            '$totalPomodoros',
+                            style: TextStyle(
+                              fontSize: 58,
+                              fontWeight: FontWeight.w600,
+                              color:
+                                  Theme.of(context).textTheme.headline1!.color,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
